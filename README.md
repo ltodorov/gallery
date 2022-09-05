b132ecab254684dafb4ce2109b7c0ef40671e38e
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2022-09-05 14:20:43 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gallery
-A script for generating static HTML galleries
+# Photo Gallery
+A script for generating static HTML photo galleries
+
+## Steps
+1. Add your photos to the `public` folder.
+2. Run `npm start`.
+3. The `index.html` inside the `public` folder should be generated.
---
+++ index.ts
@@ -0,0 +1,63 @@
+import { readdir, readFile, writeFileSync, type PathLike } from "fs";
+import { resolve } from "path";
+
+const assetsDir = resolve("public");
+const sourceFile = resolve("index.html");
+const outputFile = resolve(assetsDir, "index.html");
+
+function logError(err: any) {
+    console.error(err);
+}
+
+function readDir(path: PathLike): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        readdir(path, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(files);
+        });
+    });
+}
+
+function readTemplate(path: PathLike): Promise<string> {
+    return new Promise((resolve, reject) => {
+        readFile(path, {
+            encoding: "utf-8"
+        }, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        });
+    });
+}
+
+function getItem(file: string) {
+    return `<a href="${file}" download>
+        <img src="${file}" alt="" />
+    </a>`
+}
+
+Promise.all([
+    readDir(assetsDir),
+    readTemplate(sourceFile),
+]).then(([files, data]) => {
+    const filesHtml = files.reduce((acc, file) => acc += getItem(file), "");
+    const html = data.replace("{{root}}", filesHtml);
+    import("html-minifier-terser").then(async ({ minify }) => {
+        const result = await minify(html, {
+            collapseInlineTagWhitespace: true,
+            collapseWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: true,
+        });
+        try {
+            writeFileSync(outputFile, result);
+            console.log(`${outputFile} has been generated successfully!`);
+        } catch(err) {
+            logError(err);
+        }
+    }).catch(logError);
+}).catch(logError);
