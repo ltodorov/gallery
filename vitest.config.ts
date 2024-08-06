b78171289a8b3ed716a6880bad75a621951b6775
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2024-08-06 07:42:44 +0300

    Update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
     test: {
         coverage: {
+            include: ["src"],
             exclude: ["src/scripts/index.ts"],
             thresholds: {
                 100: true,
